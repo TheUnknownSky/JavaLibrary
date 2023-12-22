fb7d292c9d64a08895dac62bd305c60dd6d0950a
Vonn Harold Mamaril <vonnmamaril24@gmail.com>
2023-12-22 11:10:05 +0800

    fixed appointments table, cleaned db related code

--- src/DatabaseConnection/DBConn.java
+++ src/DatabaseConnection/DBConn.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 import java.sql.DriverManager;
 
 public class DBConn {
-    // change the value of database_name to the name of your database (the same name you set in testClass.java)
-    private String database_name = "testlib1";
-    
-    // run the testClass.java file to initiate the database before running the RegistrationGUI.java
+    // change the value of database_name to the desired name of your database
+    private String database_name = "testlibdb1";
+    // change the value of user and password if needed
+    private String user = "root";
+    private String password = "";
     public void initDatabaseAndTables(String db_name) throws SQLException{
-        Connection initConn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", ""); 
+        Connection initConn = DriverManager.getConnection("jdbc:mysql://localhost:3306", this.user, this.password); 
         Statement makeDB = initConn.createStatement();
         makeDB.executeUpdate("CREATE DATABASE " + db_name);
         initConn.close();
@@ -20,7 +21,7 @@ public class DBConn {
         String booksTable = "CREATE TABLE books (`book_id` INT NOT NULL AUTO_INCREMENT , `book_title` VARCHAR(255) NOT NULL , `book_author` VARCHAR(255) NOT NULL , `book_genre` VARCHAR(255) NOT NULL , `book_count` INT NOT NULL , PRIMARY KEY (`book_id`)) ENGINE = InnoDB;";        
         String bookGenreTable = "CREATE TABLE book_genre (`bg_id` INT NOT NULL AUTO_INCREMENT , `bg_name` VARCHAR(100) NOT NULL , PRIMARY KEY (`bg_id`), UNIQUE (`bg_name`)) ENGINE = InnoDB;";
         String studentTable = "CREATE TABLE students (`student_id` VARCHAR(11) NOT NULL , `student_name` VARCHAR(255) NOT NULL , PRIMARY KEY (`student_id`)) ENGINE = InnoDB;";
-        String appointmentTable = "CREATE TABLE appointments (`appt_id` INT PRIMARY KEY, `book_id` INT, `student_id` VARCHAR(11), `libacct_id` INT, `appt_date` DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (`book_id`) REFERENCES books(`book_id`), FOREIGN KEY (`student_id`) REFERENCES students(`student_id`), FOREIGN KEY (`libacct_id`) REFERENCES lib_accounts(`libacct_id`));";
+        String appointmentTable = "CREATE TABLE appointments (`appt_id` INT NOT NULL AUTO_INCREMENT , `book_id` INT NULL , `student_id` VARCHAR(11) NULL , `appt_date_borrow` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `appt_date_return` DATETIME NOT NULL , PRIMARY KEY (`appt_id`), FOREIGN KEY (`book_id`) REFERENCES books(`book_id`), FOREIGN KEY (`student_id`) REFERENCES students(`student_id`)) ENGINE = InnoDB;";
         Statement makeTable = conn.createStatement();
         makeTable.executeUpdate(accountTable);
         makeTable.executeUpdate(booksTable);
@@ -29,8 +30,16 @@ public class DBConn {
         makeTable.executeUpdate(appointmentTable);
         conn.close();
     }
-    
     public Connection conn() throws SQLException{
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.database_name, "root", "");  
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.database_name, this.user, this.password);  
+    }
+    // run this file to initiate the database before running the RegistrationGUI.java
+    public static void main(String[] args) {
+        DBConn myDb = new DBConn();
+        try {
+            myDb.initDatabaseAndTables(myDb.database_name);
+        } catch (SQLException e){
+            System.out.println(e);
+        }
     }
 }
